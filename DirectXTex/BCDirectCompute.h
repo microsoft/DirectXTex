1e0d861ee343f004c04fb0015a2d5facfa07a628
walbourn_cp <SND\walbourn_cp>
2014-09-11 15:25:42 -0700

    Minor code cleanup

--- DirectXTex/BCDirectCompute.h
+++ DirectXTex/BCDirectCompute.h
@@ -11,9 +11,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //-------------------------------------------------------------------------------------
 
-#if defined(_MSC_VER) && (_MSC_VER > 1000)
 #pragma once
-#endif
 
 namespace DirectX
 {
