639b5bb5406dd99f3c5d44baf89637884764354b
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-04-16 11:24:26 -0700

    Cleaned up code

--- DDSTextureLoader/DDSTextureLoader12.cpp
+++ DDSTextureLoader/DDSTextureLoader12.cpp
@@ -1415,12 +1415,6 @@ namespace
         _In_ ID3D12Resource** texture)
     {
 #if !defined(NO_D3D12_DEBUG_NAME) && ( defined(_DEBUG) || defined(PROFILE) )
-#if defined(_XBOX_ONE) && defined(_TITLE)
-        if (texture != 0 && *texture != 0)
-        {
-            (*texture)->SetName(fileName);
-        }
-#else
         if (texture)
         {
             const wchar_t* pstrName = wcsrchr(fileName, '\\');
@@ -1438,7 +1432,6 @@ namespace
                 (*texture)->SetName(pstrName);
             }
         }
-#endif
 #else
         UNREFERENCED_PARAMETER(fileName);
         UNREFERENCED_PARAMETER(texture);
