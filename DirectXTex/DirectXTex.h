5e44eba40d076c5434735b39aeea49354a1d9442
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-04-22 15:58:26 -0700

    Minor code review

--- DirectXTex/DirectXTex.h
+++ DirectXTex/DirectXTex.h
@@ -20,7 +20,6 @@
 #if !defined(__d3d11_h__) && !defined(__d3d11_x_h__) && !defined(__d3d12_h__) && !defined(__d3d12_x_h__)
 #if defined(_XBOX_ONE) && defined(_TITLE)
 #include <d3d11_x.h>
-#define DCOMMON_H_INCLUDED
 #else
 #include <d3d11_1.h>
 #endif
