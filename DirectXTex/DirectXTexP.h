c0cb83f9432b8e4cfff2544c4aadb576f4ae9106
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-06-12 17:36:18 -0700

    Fixed -Wmacro-redefined warning

--- DirectXTex/DirectXTexP.h
+++ DirectXTex/DirectXTexP.h
@@ -68,9 +68,12 @@
 #pragma clang diagnostic ignored "-Wunknown-pragmas"
 #endif
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #pragma warning(push)
 #pragma warning(disable : 4005)
-#define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #define NODRAWTEXT
 #define NOGDI
