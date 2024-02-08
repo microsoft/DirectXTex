e20d5b0cbc80d448543e1eafeafd9491888b8c98
Chuck Walbourn <chuckw@microsoft.com>
2024-02-07 22:35:29 -0800

    More cleanup

--- WICTextureLoader/WICTextureLoader12.cpp
+++ WICTextureLoader/WICTextureLoader12.cpp
@@ -75,6 +75,10 @@ namespace
     {
         const GUID&         wic;
         DXGI_FORMAT         format;
+
+        constexpr WICTranslate(const GUID& wg, DXGI_FORMAT fmt) noexcept :
+            wic(wg),
+            format(fmt) {}
     };
 
     constexpr WICTranslate g_WICFormats[] =
@@ -111,6 +115,10 @@ namespace
     {
         const GUID& source;
         const GUID& target;
+
+        constexpr WICConvert(const GUID& src, const GUID& tgt ) noexcept :
+            source(src),
+            target(tgt) {}
     };
 
     constexpr WICConvert g_WICConvert[] =
