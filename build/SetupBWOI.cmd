1b6cf38825dba3994ad4a089d1bd6305ae422ddd
Chuck Walbourn <chuckw@microsoft.com>
2024-05-04 11:37:52 -0700

    Use MSVC ninja

--- build/SetupBWOI.cmd
+++ build/SetupBWOI.cmd
@@ -4,30 +4,46 @@ rem Licensed under the MIT License.
 
 if %1.==. goto needpackage
 if %2.==. goto needgdk
+if %3.==PC. goto haveparams
 if %3.==XboxOne. goto haveparams
 if %3.==Scarlett. goto haveparams
 goto needconsole
 
 :haveparams
 set GXDKEDITION=%2
+echo GXDKEDITION: %GXDKEDITION%
 
 set PCNUGET=%1\Microsoft.GDK.PC.%GXDKEDITION%\
 if NOT EXIST %PCNUGET% goto missingpcnuget
+
+set GRDKLatest=%PCNUGET%native\%GXDKEDITION%\GRDK\
+echo GRDKLatest: %GRDKLatest%
+
+if %3.==PC. goto grdkonly
+
 set XBOXNUGET=%1\Microsoft.gdk.xbox.%GXDKEDITION%\
 if NOT EXIST %XBOXNUGET% goto missingxboxnuget
 
-set GRDKLatest=%PCNUGET%native\%GXDKEDITION%\GRDK\
 set GXDKLatest=%XBOXNUGET%native\%GXDKEDITION%\GXDK\
+echo GXDKLatest: %GXDKLatest%
+
 set GameDK=%XBOXNUGET%native\
 set GameDKLatest=%XBOXNUGET%native\%GXDKEDITION%\
 
 set ADDBIN=%GXDKLatest%bin\%3;%PCNUGET%native\bin;%XBOXNUGET%native\bin
 set ADDINCLUDE=%GXDKLatest%gamekit\include\%3;%GXDKLatest%gamekit\include;%GRDKLatest%gamekit\include
 set ADDLIB=%GXDKLatest%gamekit\lib\amd64\%3;%GXDKLatest%gamekit\lib\amd64;%GRDKLatest%gamekit\lib\amd64
+goto continue
 
-echo GXDKEDITION: %GXDKEDITION%
-echo GRDKLatest: %GRDKLatest%
-echo GXDKLatest: %GXDKLatest%
+:grdkonly
+set GameDK=%PCNUGET%native\
+set GameDKLatest=%PCNUGET%native\%GXDKEDITION%\
+
+set ADDBIN=%PCNUGET%native\bin
+set ADDINCLUDE=%GRDKLatest%gamekit\include
+set ADDLIB=%GRDKLatest%gamekit\lib\amd64
+
+:continue
 echo GameDK: %GameDK%
 echo GameDKLatest: %GameDKLatest%
 echo ADDBIN: %ADDBIN%
@@ -48,7 +64,7 @@ echo Usage: This script requires the GDK edition number in YYMMQQ format as the 
 exit /b 1
 
 :needconsole
-echo Usage: This script requires the console type of Scarlett or XboxOne in the third parameter
+echo Usage: This script requires the target type of PC, Scarlett, or XboxOne in the third parameter
 exit /b 1
 
 :missingpcnuget
