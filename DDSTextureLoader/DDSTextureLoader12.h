c09b4fed0d235688310c8b810e98ce645e6246e8
Chuck Walbourn <chuckw@microsoft.com>
2024-10-23 18:21:42 -0700

    Code review

--- DDSTextureLoader/DDSTextureLoader12.h
+++ DDSTextureLoader/DDSTextureLoader12.h
@@ -70,7 +70,7 @@ namespace DirectX
 #pragma clang diagnostic ignored "-Wdeprecated-dynamic-exception-spec"
 #endif
 
-    DEFINE_ENUM_FLAG_OPERATORS(DDS_LOADER_FLAGS);
+    DEFINE_ENUM_FLAG_OPERATORS(DDS_LOADER_FLAGS)
 
 #ifdef __clang__
 #pragma clang diagnostic pop
