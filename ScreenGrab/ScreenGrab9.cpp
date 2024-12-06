83bd7faa2c5dd0f5a118233514d3487a87f74841
Chuck Walbourn <chuckw@microsoft.com>
2024-12-05 21:12:36 -0800

    Assume WIC2 types always available

--- ScreenGrab/ScreenGrab9.cpp
+++ ScreenGrab/ScreenGrab9.cpp
@@ -520,7 +520,6 @@ namespace
 
     BOOL WINAPI InitializeWICFactory(PINIT_ONCE, PVOID, PVOID* ifactory) noexcept
     {
-    #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) || defined(_WIN7_PLATFORM_UPDATE)
         HRESULT hr = CoCreateInstance(
             CLSID_WICImagingFactory2,
             nullptr,
@@ -546,14 +545,6 @@ namespace
             );
             return SUCCEEDED(hr) ? TRUE : FALSE;
         }
-    #else
-        return SUCCEEDED(CoCreateInstance(
-            CLSID_WICImagingFactory,
-            nullptr,
-            CLSCTX_INPROC_SERVER,
-            __uuidof(IWICImagingFactory),
-            ifactory)) ? TRUE : FALSE;
-    #endif
     }
 
     IWICImagingFactory* GetWIC()
@@ -800,14 +791,12 @@ HRESULT DirectX::SaveWICTextureToFile(
     case D3DFMT_R32F:           pfGuid = GUID_WICPixelFormat32bppGrayFloat; break;
     case D3DFMT_A32B32G32R32F:  pfGuid = GUID_WICPixelFormat128bppRGBAFloat; break;
 
-    #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) || defined(_WIN7_PLATFORM_UPDATE)
     case D3DFMT_X8B8G8R8:
         if (g_WIC2)
             pfGuid = GUID_WICPixelFormat32bppRGB;
         else
             HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED);
         break;
-    #endif
 
     default:
         return HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED);
@@ -879,7 +868,6 @@ HRESULT DirectX::SaveWICTextureToFile(
         // Screenshots don't typically include the alpha channel of the render target
         switch (desc.Format)
         {
-        #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) || defined(_WIN7_PLATFORM_UPDATE)
         case D3DFMT_A32B32G32R32F:
         case D3DFMT_A16B16G16R16F:
             if (g_WIC2)
@@ -891,7 +879,6 @@ HRESULT DirectX::SaveWICTextureToFile(
                 targetGuid = GUID_WICPixelFormat24bppBGR;
             }
             break;
-        #endif
 
         case D3DFMT_A16B16G16R16: targetGuid = GUID_WICPixelFormat48bppBGR; break;
         case D3DFMT_R5G6B5:       targetGuid = GUID_WICPixelFormat16bppBGR565; break;
