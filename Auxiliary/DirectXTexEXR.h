ec8f82198c3a600d24d2b9c26d4343bc658be487
Chuck Walbourn <chuckw@microsoft.com>
2025-02-03 23:44:44 -0800

    Annotate aux headers for DLL

--- Auxiliary/DirectXTexEXR.h
+++ Auxiliary/DirectXTexEXR.h
@@ -14,13 +14,15 @@
 
 namespace DirectX
 {
-    HRESULT __cdecl GetMetadataFromEXRFile(
+    DIRECTX_TEX_API HRESULT __cdecl GetMetadataFromEXRFile(
         _In_z_ const wchar_t* szFile,
         _Out_ TexMetadata& metadata);
 
-    HRESULT __cdecl LoadFromEXRFile(
+    DIRECTX_TEX_API HRESULT __cdecl LoadFromEXRFile(
         _In_z_ const wchar_t* szFile,
         _Out_opt_ TexMetadata* metadata, _Out_ ScratchImage& image);
 
-    HRESULT __cdecl SaveToEXRFile(_In_ const Image& image, _In_z_ const wchar_t* szFile);
+    DIRECTX_TEX_API HRESULT __cdecl SaveToEXRFile(
+        _In_ const Image& image,
+        _In_z_ const wchar_t* szFile);
 }
