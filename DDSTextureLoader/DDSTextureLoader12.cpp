3bbde957eb0d946207660447f1114d7bae1d06c0
karurung <84292079+karurung@users.noreply.github.com>
2025-04-12 21:21:20 -0700

    Update DDSTextureLoader12.cpp
    The original comment shows there is no DXGI format, so I used `ISBITMASK(0x000000ff, 0x0000ff00, 0x00ff0000, 0)`. This format doesn't have alpha, and I'm sure this is 32 bit. Therefore, in DX12, I think `DXGI_FORMAT_B8G8R8X8_UNORM`, which also helps to go SRGB. (I deleted the commet)

--- DDSTextureLoader/DDSTextureLoader12.cpp
+++ DDSTextureLoader/DDSTextureLoader12.cpp
@@ -768,7 +768,6 @@ namespace
                     return DXGI_FORMAT_B8G8R8X8_UNORM;
                 }
 
-                // No DXGI format maps to ISBITMASK(0x000000ff,0x0000ff00,0x00ff0000,0) aka D3DFMT_X8B8G8R8
                 if (ISBITMASK((0x000000ff,0x0000ff00,0x00ff0000,0))
                 {
                     return DXGI_FORMAT_B8G8R8X8_UNORM;
